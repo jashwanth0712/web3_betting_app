2ec7ea4b6491a1a96f9f5633a4680806cde7511e
jashwanth0712 <jashwanth0712@gmail.com>
2022-12-18 19:40:06 +0530

    updated the clients section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import EventDetails from './pages/EventDetails';
+import CreateEvent from './pages/CreateEvent';
+import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
-    <div>
-      hello world
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-Event" element={<CreateEvent />} />
+          <Route path="/Event-details/:id" element={<EventDetails />} />
+        </Routes>
+      </div>
     </div>
   )
 }
